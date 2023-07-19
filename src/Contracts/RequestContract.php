a2e79b7f860f699e341c5dc4ea10330651c50efb
if3chi <if3chi@users.noreply.github.com>
2023-07-19 00:25:21 +0000

    Fix styling

--- src/Contracts/RequestContract.php
+++ src/Contracts/RequestContract.php
@@ -9,16 +9,28 @@ use JustSteveKing\UriBuilder\Uri;
 interface RequestContract
 {
     public function headers(array $headers = []): array;
+
     public function payload(array $payload = []): array;
+
     public function parameters(array $parameters = []): array;
+
     public function method(): string;
+
     public function uri(): Uri;
+
     public function requiesAuth(): bool;
+
     public function authStrategy(): ?string;
+
     public function authCredentials(): null|string|array;
+
     public function retry(): int;
+
     public function retryTiming(): float;
+
     public function timeout(): float;
-    public function path(?string $path = null): ?string;
+
+    public function path(string $path = null): ?string;
+
     public function fragment(): ?string;
 }
