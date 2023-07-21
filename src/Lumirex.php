83ab7a5169c77197160444e7607a027fba71e3e0
Ifechi <j.cyxtus@gmail.com>
2023-07-21 22:59:33 +0100

    Added send()

--- src/Lumirex.php
+++ src/Lumirex.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace If3chi\Lumirex;
 
 use If3chi\Lumirex\Concerns\HasFake;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\PendingRequest;
 use If3chi\Lumirex\Contracts\RequestContract;
@@ -20,6 +21,23 @@ class Lumirex
     public static function request(RequestContract $request): self
     {
         return new self(request: $request);
+    }
+
+    public function send(): Response
+    {
+        $request = $this->buildRequest();
+
+        $response = $request->send(
+            method: $this->request->method(),
+            url: $this->request->uri()->toString(),
+            options: $this->request->payload()
+        );
+
+        if ($response->failed()) {
+            return $response->toException();
+        }
+
+        return $response;
     }
 
     public function with(array $payload = [], array $headers = [], ?string $path = null): self
