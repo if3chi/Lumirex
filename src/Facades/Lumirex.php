d3717b2b6674e688406af5e45af38fac1f3fa04e
Ifechi <j.cyxtus@gmail.com>
2023-07-21 22:42:56 +0100

    Refactor

--- src/Facades/Lumirex.php
+++ src/Facades/Lumirex.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace If3chi\Lumirex\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use If3chi\Lumirex\Lumirex as BaseLumirex;
 
 /**
  * @see \If3chi\Lumirex\Lumirex
@@ -13,6 +14,6 @@ class Lumirex extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return \If3chi\Lumirex\Lumirex::class;
+        return BaseLumirex::class;
     }
 }
